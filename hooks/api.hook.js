4c8cb46f131a7d22a4c6a9a2544153f68b05cc4f
AnnaRogojine <anarogozin@gmail.com>
2021-04-13 20:09:50 +0300

    Added hook for api

---
+++ hooks/api.hook.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+import { useMemo } from 'react';
+
+
+
+export function useApi() {
+
+    const httpClient = useMemo(() => axios.create({
+        baseURL: 'http://192.168.1.22:3000/api'
+    }), [])
+
+    const login = async () => await httpClient.get('/whatever')
+    const getHouseDetails = async houseId => (await httpClient.get(`/houses/${houseId}`)).data
+    const deleteProduct = async (houseId, productId) => (await httpClient.put(`/houses/DeleteProduct/${houseId}/${productId}`))
+    const updateQuantity = async (houseId, productId, Quantity) => (await httpClient.put(`/houses/updateQuantity/${houseId}/${productId}/${Quantity}`))
+
+    const getSupermarkets = async (lat, long) => (await httpClient.get('/maps/supermarkets', { params: { lat, long } })).data
+    return { login, getHouseDetails, deleteProduct, updateQuantity, getSupermarkets };
+
+}
